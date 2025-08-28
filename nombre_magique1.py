5b3eeb4fc5970cf631855147e51321e6245a5ecb
unknown <kaborejbaime@gmail.com>
2025-08-28 20:27:22 +0200

    Training nombre magique

---
+++ nombre_magique1.py
@@ -0,0 +1,29 @@
+
+def demander_nombre(nb_min, nb_max):
+    nombre_int = 0
+    while nombre_int == 0:
+        nombre_str = input(f"Quel est le nombre magique ? (entre {nb_min} et {nb_max})")
+        try:        
+            nombre_int = int(nombre_str)
+        except:
+            print("ERREUR, Vous devez renseigner un nombre. Réessayez")
+    return nombre_int
+
+
+NOMBRE_MIN = 1
+NOMBRE_MAX = 10
+NOMBRE_MAGIQUE = 5
+
+
+
+nombre = 0
+
+while not nombre == NOMBRE_MAGIQUE:
+    nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
+    if nombre == NOMBRE_MAGIQUE:
+        print("Bravo, vous avez gagné")
+    elif nombre > NOMBRE_MAGIQUE:
+        print("Le nombre magique est plus petit")
+    else:
+        print("Le nombre magique est plus grand")
+    
