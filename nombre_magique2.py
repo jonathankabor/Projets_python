41f9e403b06cbdd2b75fcf7e260f5b588063bdf5
unknown <kaborejbaime@gmail.com>
2025-08-30 18:41:46 +0200

    Demander le nombre

---
+++ nombre_magique2.py
@@ -0,0 +1,15 @@
+
+
+def demander_nombre(nb_min, nb_max):
+    nombre_int = 0
+    nombre_str = input(f"Quel est le nombre magique (entre {nb_min} et {nb_max}) ?")
+    nombre_int = int(nombre_str)
+    return nombre_int
+
+
+NOMBRE_MIN = 1
+NOMBRE_MAX = 10
+NOMBRE_MAGIQUE = 5
+
+
+nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
