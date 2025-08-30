496827916da1141011efe45efd2e1c5c37cbcfeb
unknown <kaborejbaime@gmail.com>
2025-08-30 19:16:21 +0200

    Boucle while

--- nombre_magique2.py
+++ nombre_magique2.py
@@ -1,7 +1,6 @@
 
 
 def demander_nombre(nb_min, nb_max):
-    
     nombre_str = input(f"Quel est le nombre magique (entre {nb_min} et {nb_max}) ?")
     nombre_int = int(nombre_str)
     return nombre_int
@@ -13,13 +12,13 @@ NOMBRE_MAGIQUE = 5
 
 
 
-
-nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
-
-if nombre == NOMBRE_MAGIQUE:
-    print("Bravo, vous avez gagné")
-elif nombre > NOMBRE_MAGIQUE:
-    print("Le nombre magique est plus petit")
-else:
-    print("Le nombre magique est plus grand")
+nombre = 0
+while not nombre == NOMBRE_MAGIQUE:
+    nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
+    if nombre == NOMBRE_MAGIQUE:
+        print("Bravo, vous avez gagné")
+    elif nombre > NOMBRE_MAGIQUE:
+        print("Le nombre magique est plus petit")
+    else:
+        print("Le nombre magique est plus grand")
     
