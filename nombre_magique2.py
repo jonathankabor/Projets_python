426b12532a1d30e1618eec3bb9b159ad4b2bf2fa
unknown <kaborejbaime@gmail.com>
2025-08-31 18:56:07 +0200

    Limiter le nombre de vies avec la boucle while

--- nombre_magique2.py
+++ nombre_magique2.py
@@ -1,4 +1,4 @@
-
+import random
 
 def demander_nombre(nb_min, nb_max):
     nombre_int = 0
@@ -7,23 +7,35 @@ def demander_nombre(nb_min, nb_max):
         try:
             nombre_int = int(nombre_str)
         except:
-            print("ERREUR: Vous devez rentrer un nombre. Réessayez")   
+            print("ERREUR: Vous devez rentrer un nombre. Réessayez")  
+        else:
+            if nombre_int < nb_min or nombre_int > nb_max:
+                print(f"ERREUR: Le nombre doit être entre {nb_min} et {nb_max}. Réessayez.")
+                nombre_int = 0     
     return nombre_int
 
 
 NOMBRE_MIN = 1
 NOMBRE_MAX = 10
-NOMBRE_MAGIQUE = 5
-
+NOMBRE_MAGIQUE = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+NB_VIES = 4
 
 
 nombre = 0
-while not nombre == NOMBRE_MAGIQUE:
+vies = NB_VIES
+
+while not nombre == NOMBRE_MAGIQUE and vies > 0 :
+    print(f"Il vous reste {vies} vies")
     nombre = demander_nombre(NOMBRE_MIN, NOMBRE_MAX)
     if nombre == NOMBRE_MAGIQUE:
         print("Bravo, vous avez gagné")
     elif nombre > NOMBRE_MAGIQUE:
         print("Le nombre magique est plus petit")
+        vies -= 1
     else:
         print("Le nombre magique est plus grand")
+        vies -= 1
+        
+if vies == 0:
+    print(f"Vous avez perdu, le nombre magique était: {NOMBRE_MAGIQUE}")
     
