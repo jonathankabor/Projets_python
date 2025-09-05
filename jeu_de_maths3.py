3f51f86f6c23f7bfd37f164e0c94a4c0a476c755
unknown <kaborejbaime@gmail.com>
2025-09-05 18:50:26 +0200

    Révision addition et Plusieurs questions

---
+++ jeu_de_maths3.py
@@ -0,0 +1,22 @@
+import random 
+
+
+NOMBRE_MIN = 1
+NOMBRE_MAX = 10
+NB_QUESTIONS = 4
+
+def poser_question():
+    a = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+    b = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+    reponse_str = input(f"Calculez : {a} + {b} = ")
+    reponse_int = int(reponse_str)
+    if reponse_int == a+b:
+        print("Réponse correcte")
+    else:
+        print("Réponse incorrecte")
+        
+
+for i in range(0, NB_QUESTIONS):   
+    print(f" Question n° {i+1} sur {NB_QUESTIONS}: ") 
+    poser_question()
+    print()
