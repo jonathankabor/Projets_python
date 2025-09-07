fa5a81b8cd9d1c66d601a2028c2d10925c0c531a
unknown <kaborejbaime@gmail.com>
2025-09-07 19:51:49 +0200

    entrainement exem

--- training.py
+++ training.py
@@ -1,1 +1,28 @@
-print("Je m'appelle" + "Toto")
+a = 10
+print(a > 10)
+if a <= 10:
+    a = a + 5
+else:
+    a = a - 5
+if a > 10:
+    print("Supérieur à 10")
+else:
+    print("Inférieur ou égal à 10")
+if a == 10:
+    print("Egal à 10")
+if a == 15:
+    print("Egal à 15")
+if a == 5:
+    print("Egal à 5")
+else:
+    print("Différent de 5")
+    
+    
+a = 5
+ 
+def secret(b):
+    print(b+b)
+ 
+print(a)
+secret("a")
+secret(a)
