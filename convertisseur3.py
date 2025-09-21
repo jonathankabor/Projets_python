c45c48e2517ff754e0edacd14450172fcb80e4e8
unknown <kaborejbaime@gmail.com>
2025-09-21 19:07:56 +0200

    ajout de except ValueError

--- convertisseur3.py
+++ convertisseur3.py
@@ -2,13 +2,18 @@
 
 
 # Conversion de unit1 vers unit2
-# return True : l'utilisateur veut quitter
+# return True : l'utilisateur ne veut plus convertir
+# la fonction effectuer_conversion : convertit les unités unit1 vers unit2
 
 def effectuer_conversion(unit1: str, unit2: str, facteur: float):
     valeur_str = input(f"Conversion {unit1} -> {unit2}. Donnez la valeur en {unit1} (ou 'q' pour quitter): ")
     if valeur_str == "q":
         return True
-    valeur_float = float(valeur_str)
+    try:
+        valeur_float = float(valeur_str)
+    except ValueError:
+        print("ERREUR: Vous devez rentrer une valeur numérique (utiliser le point et non la virgule pour les décimales)")
+        
     valeur_convertie = round(valeur_float * 2.54, 2)
     print(f"Résultat de la conversion: {valeur_float} {unit1} = {valeur_convertie} {unit2} ")
     return False
@@ -21,7 +26,9 @@ choix = input("Votre choix (1 ou 2): ")
 
 while True:
     if choix == "1":
-        effectuer_conversion("pouces", "cm", 2.54)
+        if effectuer_conversion("pouces", "cm", 2.54):
+            break
         
     if choix == "2":
-        effectuer_conversion("cm", "pouces", 0.394)
+        if effectuer_conversion("cm", "pouces", 0.394):
+            break
