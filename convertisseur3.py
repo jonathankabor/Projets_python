9178e65d57a599b7e149256f146580a10d26076c
unknown <kaborejbaime@gmail.com>
2025-09-21 18:58:53 +0200

    ajout de la possibilité de quitter le programme

--- convertisseur3.py
+++ convertisseur3.py
@@ -2,11 +2,16 @@
 
 
 # Conversion de unit1 vers unit2
+# return True : l'utilisateur veut quitter
+
 def effectuer_conversion(unit1: str, unit2: str, facteur: float):
-    valeur_str = input(f"Conversion {unit1} -> {unit2}. Donnez la valeur en {unit1} ou 'q' pour sortir: ")
+    valeur_str = input(f"Conversion {unit1} -> {unit2}. Donnez la valeur en {unit1} (ou 'q' pour quitter): ")
+    if valeur_str == "q":
+        return True
     valeur_float = float(valeur_str)
     valeur_convertie = round(valeur_float * 2.54, 2)
     print(f"Résultat de la conversion: {valeur_float} {unit1} = {valeur_convertie} {unit2} ")
+    return False
     
 print("Ce programme vous permet d'effectuer des conversions d'unités")
 print("1 - Pouces vers cm")
