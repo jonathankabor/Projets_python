fc6ef80c47783af0377d9ae67446471423c9ef26
unknown <kaborejbaime@gmail.com>
2025-09-28 19:36:22 +0200

    rajout d'une fonction de gestion

--- convertisseur3.py
+++ convertisseur3.py
@@ -40,25 +40,54 @@ def demander_et_afficher_conversion(unit1: str, unit2: str, facteur: float, reve
     print(f"Résultat de la conversion: {valeur_float} {unit1} = {valeur_convertie} {unit2} ")
     return False
 
-while True:
-    # menu : choix de la conversion
-    print("Ce programme vous permet d'effectuer des conversions d'unités")
-    i = 1
-    for c in CONVERSIONS:
-        unit1 = c[0]
-        unit2 = c[1]
-        print(f"{i} - {unit1} vers {unit2}")
-        i+=1
+def demander_valeur_numerique_utilisateur(valeur_min, valeur_max):
+    v = input(f"Donnez une valeur entre {valeur_min} et {valeur_max} : ")
+    try:
+        v_int = int(v)
+    except:
+        print("ERREUR: Vous devez rentrer une valeur numérique")
+        return demander_valeur_numerique_utilisateur(valeur_min, valeur_max)
+    
+    if not valeur_min <= v_int <= valeur_max:
+        print(f"ERREUR: Votre valeur doit être entre {valeur_min} et {valeur_max}")
+        return demander_valeur_numerique_utilisateur(valeur_min, valeur_max)
+    
+    return v_int
 
+#while True:
+# menu : choix de la conversion
+print("Ce programme vous permet d'effectuer des conversions d'unités")
+i = 1
+for c in CONVERSIONS:
+    unit1 = c[0]
+    unit2 = c[1]
+    print(f"{i} - {unit1} vers {unit2}")
+    i+=1
+    print(f"{i} - {unit2} vers {unit1}")
+    i+=1
     #print("1 - Pouces vers cm")
     #print("2 - cm vers Pouces")
-    choix = input("Votre choix (1 ou 2): ")
-    if (choix == "1" or choix == "2"):
-        break
+valeur_choix_maximale = i-1
+        
+choix_int = demander_valeur_numerique_utilisateur(1, valeur_choix_maximale)
+
+    #if 1 <= choix_int <= valeur_choix_maximale:
+    #break
     
-    print("ERREUR : Vous devez choisir 1 ou 2\n")
+    #print(f"ERREUR : Vous devez choisir une valeur entre 1 et {valeur_choix_maximale}\n")
+    #choix = input(f"Votre choix (entre 1 et {valeur_choix_maximale}): ")
+
+    #try:
+        #choix_int = int(choix)
+    #except:
+        #print("ERREUR: Vous devez entrer une valeur numérique")
+        #continue #pour revenir au début du menu
+
+
+   
     
-    
+
+   
 while True:
     # Demander les valeurs à convertir à l'utilisateur
     #if choix == "1":
