927ceeb384b7364471c375882b88cdafa29403a7
unknown <kaborejbaime@gmail.com>
2025-09-28 18:44:38 +0200

    améliration qualitative du code

--- convertisseur3.py
+++ convertisseur3.py
@@ -5,7 +5,11 @@
 # return True : l'utilisateur ne veut plus convertir
 # la fonction effectuer_conversion : convertit les unités unit1 vers unit2
 
-def demander_et_afficher_conversion(unit1: str, unit2: str, facteur: float):
+def demander_et_afficher_conversion(unit1: str, unit2: str, facteur: float, reverse: bool = False):
+    if reverse:
+        unit1, unit2 = unit2, unit1
+        facteur = 1 / facteur
+        
     valeur_str = input(f"Conversion {unit1} -> {unit2}. Donnez la valeur en {unit1} (ou 'q' pour quitter): ")
     if valeur_str == "q":
         return True
@@ -15,7 +19,7 @@ def demander_et_afficher_conversion(unit1: str, unit2: str, facteur: float):
         print("ERREUR: Vous devez rentrer une valeur numérique \n utiliser le point et non la virgule pour les décimales)")
         return demander_et_afficher_conversion(unit1, unit2, facteur)
         
-    valeur_convertie = round(valeur_float * 2.54, 2)
+    valeur_convertie = round(valeur_float * facteur, 2)
     print(f"Résultat de la conversion: {valeur_float} {unit1} = {valeur_convertie} {unit2} ")
     return False
 
@@ -33,10 +37,11 @@ while True:
     
 while True:
     # Demander les valeurs à convertir à l'utilisateur
-    if choix == "1":
-        if demander_et_afficher_conversion("pouces", "cm", 2.54):
+    #if choix == "1":
+        if demander_et_afficher_conversion("pouces", "cm", 2.54, reverse=False if choix == "1" else True):
             break
         
-    if choix == "2":
-        if demander_et_afficher_conversion("cm", "pouces", 0.394):
-            break
+    #if choix == "2":
+        #if demander_et_afficher_conversion("pouces", "cm", 2.54, reverse=True):
+        #if demander_et_afficher_conversion("cm", "pouces", 0.394):
+            #break
