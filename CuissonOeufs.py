30b2dd3fd35e00e9f81133690fc3781bacaf9660
unknown <kaborejbaime@gmail.com>
2025-10-11 19:04:34 +0200

    choix de la durée de cuisson

--- CuissonOeufs.py
+++ CuissonOeufs.py
@@ -14,4 +14,21 @@ print("1) Oeufs à la coque : 3 minutes")
 print("2) Oeufs mollets : 6 minutes")
 print("3) Oeufs durs : 9 minutes")
 
+
 choix = input("Quel est votre choix ? :")
+
+while True:
+    if choix == '1':
+        cuisson1 = "3"
+        print(f"Vous avez choisit une cuisson de {cuisson1} minutes pour vos Oeufs à la coque")
+        break
+    elif choix == '2':
+        cuisson2 = "6"
+        print(f"Vous avez choisit une cuisson de {cuisson2} minutes pour vos Oeufs mollets")
+        break
+    elif choix == '9':
+        cuisson3 = "540"
+        print(f"Vous avez choisit une cuisson de {cuisson3} minutes pour vos Oeufs durs")
+        break
+    else:
+        print("Choix invalide veuillez rentrer '1', '2' ou '3' .")
