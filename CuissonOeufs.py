f347975c048bc7420afd7a3384a0cdc08f462191
unknown <kaborejbaime@gmail.com>
2025-10-11 22:07:24 +0200

    amélioration du programme

--- CuissonOeufs.py
+++ CuissonOeufs.py
@@ -23,59 +23,66 @@ def cuisson_oeufs_decompte():
     cui3 = "9 minutes"
     # Démarrer un compte à rebours pour la cuisson des oeufs et afficher le temps restant toutes les 10 secondes
     # Convertir la durée en seconde
-    duree_secondes1 = 3 * 60
-    duree_secondes2 = 6 * 60 
-    duree_secondes3 = 9 * 60
+    duree_secondes1 = 180 // 60
+    duree_secondes2 = 360 // 60 
+    duree_secondes3 = 540 // 60
     # Définir l'intervalle d'affichage   
     intervalle_affichage = 10 
     if duree_secondes1:  
-        print(f"Cuisson de oeufs lancée pour {duree_secondes1} secondes...")
+        print(f"Cuisson de oeufs lancée pour {duree_secondes1} minutes...")
     elif duree_secondes2:
-        print(f"Cuisson de oeufs lancée pour {duree_secondes2} secondes...")
+        print(f"Cuisson de oeufs lancée pour {duree_secondes2} minutes...")
     else:
-        print(f"Cuisson de oeufs lancée pour {duree_secondes3} secondes...")
+        print(f"Cuisson de oeufs lancée pour {duree_secondes3} minutes...")
         
-    temps_restant = duree_secondes1
-    temps_restant = duree_secondes2
-    temps_restant = duree_secondes3
+    temps_restant1 = duree_secondes1
+    temps_restant2 = duree_secondes2
+    temps_restant3 = duree_secondes3
     
     
     
     #Boucle compte à rebours
     while True:
-        if (temps_restant % intervalle_affichage == 0) or (temps_restant == duree_secondes1):
-            minutes = temps_restant // 60
-            secondes = temps_restant % 60
-            print(f"Le Temps restant est de {cui1}: ")
-            temps_restant -=1
-            if temps_restant > 0:
-                for secondes in range(1, duree_secondes1):
-                    time.sleep(1)
-                    print(".", end="", flush=True)
-            break
-        elif (temps_restant % intervalle_affichage == 0) or (temps_restant == duree_secondes2):
-            minutes = temps_restant // 60
-            secondes = temps_restant % 60
-            print(f"Le Temps restant est de {cui2}: ")
-            temps_restant -=1
-            if temps_restant > 0:
+        point_par_ligne = 5
+        if temps_restant1 > 0:
+            if (temps_restant1 % intervalle_affichage == 0) or (temps_restant1 == duree_secondes1):
                 for i in range(5):
                     time.sleep(1)
                     print(".", end="", flush=True)
-            break
-        elif (temps_restant % intervalle_affichage == 0) or (temps_restant == duree_secondes3):
-            minutes = temps_restant // 60
-            secondes = temps_restant % 60
-            print(f"Le Temps restant est de {cui3}: ")
-            temps_restant -=1
-            if temps_restant > 0:
+                    print("Cuisson en cours")
+                    if duree_secondes1 % point_par_ligne == 0 and temps_restant1 > duree_secondes1:
+                        print(f"Le Temps restant est de {cui1}: {duree_secondes1:02d} minutes")
+                        print()
+                        temps_restant1 -=1
+                        break
+                    
+        elif (temps_restant2 % intervalle_affichage == 0) or (temps_restant2 == duree_secondes2):
+            if temps_restant2 > 0:
                 for i in range(5):
                     time.sleep(1)
                     print(".", end="", flush=True)
-            
+                    print("Cuisson en cours")
+                    if duree_secondes2 % point_par_ligne == 0 and temps_restant2 > duree_secondes2:
+                        print(f"Le Temps restant est de {cui2}: {duree_secondes2:02d} minutes")
+                        print()
+                        temps_restant2 -=1
+                        break
+                    
+        elif (temps_restant3 % intervalle_affichage == 0) or (temps_restant3 == duree_secondes3):
+            if temps_restant3 > 0:
+                for i in range(5):
+                    time.sleep(1)
+                    print(".", end="", flush=True)
+                    print("Cuisson en cours")
+                    if duree_secondes3 % point_par_ligne == 0 and temps_restant3 > duree_secondes3:
+                        print(f"Le Temps restant est de {cui3}: {duree_secondes3:02d} minutes")
+                        print()
+                        temps_restant3 -=1
+                        break
+                    
        
-        
-    print("\n C'est prêt ! Vos oeufs sont cuits. Bon appétit")
+    
+print("\n C'est prêt ! Vos oeufs sont cuits. Bon appétit")
     
 while True:
     if choix == '1':
