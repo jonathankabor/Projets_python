e0d0d1f7e8c02571098cd1e4d1d4028bd296c72d
unknown <kaborejbaime@gmail.com>
2025-10-11 18:33:30 +0200

    conception du menu

---
+++ CuissonOeufs.py
@@ -0,0 +1,17 @@
+import time
+
+"""
+   Votre programme proposera 3 options :
+   1) Oeufs à la coque : 3 minutes
+   2) Oeufs mollets : 6 minutes
+   3) oeufs durs : 9 minutes 
+    
+"""
+print("***********************")
+print("Choisissez votre Menu")
+print("***********************")
+print("1) Oeufs à la coque : 3 minutes")
+print("2) Oeufs mollets : 6 minutes")
+print("3) Oeufs durs : 9 minutes")
+
+choix = input("Quel est votre choix ? :")
