7a12e4f22a7f7822966c171ade1d5641dfe55aca
unknown <kaborejbaime@gmail.com>
2025-10-12 18:19:51 +0200

    ajout des choix de temps et du menu

---
+++ cuissonOeufs1.py
@@ -0,0 +1,38 @@
+
+
+
+"""
+- Oeufs à la coque : 3 minutes
+- Oeufs mollets : 6 minutes
+- Oeufs durs : 9 minutes
+
+import time
+time.sleep(1)
+print(".", end="", flush=True)
+
+d = 100
+min = d//60 # division entière (pas de virgules)
+sec = d-min*60
+
+print(f"{min:02d}")
+    
+"""
+import time
+
+print("Cuisson des oeufs")
+print("1 - Oeufs à la coque : 3 minutes")
+print("2 - Oeufs mollets : 6 minutes")
+print("3 - Oeufs durs : 9 minutes")
+choix = input("Votre choix : ")
+
+duree = 0
+if choix == "1":
+    duree = 3 * 60
+if choix == "2":
+    duree = 6 * 60
+if choix == "3":
+    duree = 9 * 60
+    
+while True:
+    time.sleep(1)
+    print(".", end="", flush=True)
