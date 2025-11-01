e1d2b0e1f8d0f377879679825a7d2c2507fae5a6
unknown <kaborejbaime@gmail.com>
2025-11-01 18:30:23 +0100

    définition des conditions de collection

---
+++ simon2.py
@@ -0,0 +1,71 @@
+"""
+- Longueur séquence initiale : 4
+- Durée de mémorisation : 3s
+- A chaque tour : 1 chiffre
+- Un nombre de vies
+
+Facile
+    longueur_initiale : 3
+    duree_memorisation : 4
+    increment_sequence : 1
+    nombre_vies : 2
+
+Normal
+    longueur_initiale : 4
+    duree_memorisation : 3
+    increment_sequence : 1
+    nombre_vies : 1
+
+Difficile
+    longueur_initiale : 5
+    duree_memorisation : 2
+    increment_sequence : 2
+    nombre_vies : 0
+
+"""
+
+
+import random
+import time
+import os
+
+
+def clear_screen():
+    if(os.name == 'posix'):
+        os.system('clear')
+    else:
+        os.system('cls')
+        
+        
+
+
+# Génération de la séquence initiale
+sequence = ""
+for i in range(4):
+    chiffre = random.randint(0,9)
+    sequence += str(chiffre)
+
+clear_screen() 
+print("Bienvenue dans le jeu du Simon") 
+
+score = 0
+while True:
+    print("Retenez la séquence")
+    time.sleep(1)
+    print(sequence)
+    time.sleep(3)
+    clear_screen()
+
+    seq_utilisateur = input("Rentrez la séquence: ")
+    if seq_utilisateur == sequence:
+        score +=1
+    else:
+        break
+
+    chiffre = random.randint(0,9)
+    sequence += str(chiffre)
+    clear_screen()
+    
+print("Mauvaise réponse")
+print(f"La réponse était {sequence}")
+print(f"Votre score final est : {score}")
