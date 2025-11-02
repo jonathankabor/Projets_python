74a5ea3f73baea9893279bda347a14e0c14f9e45
unknown <kaborejbaime@gmail.com>
2025-11-02 17:41:08 +0100

    Appels de fonctions

---
+++ fonctions.py
@@ -0,0 +1,6 @@
+# Les fonctions
+
+nom = "Jo le Magicien"
+print("Je m'appelle " + nom) # concaténé la chaine # <-- 1 seul paramètre
+print()
+print("Je m'appelle", nom, "J'ai", 22, "ans") # <-- 2 paramètres
