4d49776f986441aa8dd5bc0cb9f979ee81441fb0
unknown <kaborejbaime@gmail.com>
2025-11-02 17:45:42 +0100

    fonction input

--- fonctions.py
+++ fonctions.py
@@ -4,3 +4,7 @@ nom = "Jo le Magicien"
 print("Je m'appelle " + nom) # concaténé la chaine # <-- 1 seul paramètre
 print()
 print("Je m'appelle", nom, "J'ai", 22, "ans") # <-- 2 paramètres
+
+input("Votre nom :") # <-- Retourne une valeur (La fonction input bloque le programme tant qu'on a pas donné 
+# une réponse)
+print("Votre nom est:", nom)
