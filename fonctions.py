27a49cf1636ba0cace435496aa89c0aafa7fd4d3
unknown <kaborejbaime@gmail.com>
2025-11-02 17:54:06 +0100

    les possibilités de la fonction input

--- fonctions.py
+++ fonctions.py
@@ -1,5 +1,6 @@
 # Les fonctions
 
+""" 
 nom = "Jo le Magicien"
 print("Je m'appelle " + nom) # concaténé la chaine # <-- 1 seul paramètre
 print()
@@ -8,3 +9,15 @@ print("Je m'appelle", nom, "J'ai", 22, "ans") # <-- 2 paramètres
 input("Votre nom :") # <-- Retourne une valeur (La fonction input bloque le programme tant qu'on a pas donné 
 # une réponse)
 print("Votre nom est:", nom)
+
+nom1 = input("Nom de la personne 1: ")
+age1 = input("Age de la personne 1: ")
+print("La personne 1 est", nom1, "son age est", age1, "ans")
+print("le nom comporte", len(nom1), "lettres")
+
+nom2 = input("Nom de la personne 2: ")
+age2 = input("Age de la personne 2: ")
+print("La personne 1 est", nom2, "son age est", age2, "ans")
+print("le nom comporte", len(nom2), "lettres")
+
+"""
