dadf034e00607ed76c5b8d649b0a8e31621a04b2
unknown <kaborejbaime@gmail.com>
2025-11-06 19:47:46 +0100

    rajout de la variable age

--- fonctions.py
+++ fonctions.py
@@ -136,10 +136,10 @@ recuperer_et_afficher_infos_personne
 def recuperer_infos_personne(numero_personne):
     nom_personne = input("Nom de la personne " + str(numero_personne) + ": ")
     age_personne = input("Age de la personne " + str(numero_personne) + ": ")
-    return nom_personne
+    return nom_personne, age_personne
     
     
-def afficher_infos_personne(numero_personne,nom, age=0):
+def afficher_infos_personne(numero_personne,nom, age):
     print("La personne ", numero_personne,  "est", nom, "son age est", age, "ans")
     print("le nom comporte", len(nom), "caractères")
 # implémenter recuperer_et_afficher_infos_personne
@@ -148,8 +148,8 @@ def afficher_infos_personne(numero_personne,nom, age=0):
 # input / print
 
 def recuperer_et_afficher_infos_personne(numero_personne):
-    nom = recuperer_infos_personne(numero_personne)
-    afficher_infos_personne(numero_personne, nom)
+    nom, age = recuperer_infos_personne(numero_personne)
+    afficher_infos_personne(numero_personne, nom, age)
     
 
 
