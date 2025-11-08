f61643b22c42908614a4e5b24a62a007fa6c6706
unknown <kaborejbaime@gmail.com>
2025-11-08 19:17:40 +0100

    Refactorisation du code

--- questionnaireFonction1.py
+++ questionnaireFonction1.py
@@ -14,7 +14,7 @@ Question : Quelle est la capitale de l'Italie
 ...
 4 questions
 """
-
+"""
 print("Quelle est la capitale de la France ?")
 print("(a) Marseille")
 print("(b) Nice")
@@ -39,4 +39,18 @@ if reponse == "a":
     print("Bonne réponse")
 else:
     print("Mausaive réponse")
+"""
+
+def poser_question(question, r1, r2, r3, r4, choix_bonne_reponse):
+    print(question)  
+    print(r1)
+    print(r2)
+    print(r3)
+    print(r4)
+    reponse = input("Votre réponse :")
+    if reponse == choix_bonne_reponse:
+        print("Bonne réponse")
+    else:
+        print("Mausaive réponse")
     
+poser_question("Question : Quelle est la capitale de la france ?", "(a) Marseille", "(b) Nice", "(c) Paris", "(d) Nantes", "c")
