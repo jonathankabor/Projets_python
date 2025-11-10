22d4e347c4aad803208eb320835f1c113a3cbcab
unknown <kaborejbaime@gmail.com>
2025-11-10 18:09:03 +0100

    gestion des tuples

---
+++ collection1.py
@@ -0,0 +1,25 @@
+# Collections : (Tableaux : Array), Listes, Tuples...
+# Tuple (): immutable --> Non modifiable
+# Liste []: mutable --> modifiable : rajouter/supprimer des éléments ou les modifier
+# Plusieurs éléments
+
+
+# ------------------------ Les Tuples ------------------------------
+# personnes = ("Mélanie", "Jean", "Martin", "Alice")
+# print(len(personnes))
+# print(personnes[-1]) # -1 pour récupérer le dernier élément
+
+# for i in range(0, len(personnes)):
+#    print(personnes[i])
+
+# for i in personnes:
+#    print(i)
+#    print(len(i))
+#    print(i[-1])
+
+# (0, 1, 2, 3, 4)
+# valeurs =range(0, len(5))
+# print(valeurs)
+# print(valeurs[-1])
+
+# ---------------------------- Les Listes -------------------------------
