6301022f98ba1b2e61548bdd75819b5ee2115800
unknown <kaborejbaime@gmail.com>
2025-11-11 19:25:32 +0100

    gestion de la liste des pizzas

--- colPizza1.py
+++ colPizza1.py
@@ -1,15 +1,24 @@
 # collection pizza
 # afficher les pizza par ligne
+# "AUCUNE PIZZA"
 
 # --------------- LISTE DES PIZZAS (4) -----------------------
 
 def afficher_pizza(collection):
     nb_pizzas = len(collection)
+    if nb_pizzas == 0:
+        print("AUCUNE PIZZA")
+        return
+        
     print(f"---- LISTE DES PIZZAS ({(nb_pizzas)})----")
     for i in collection:
         print(i)
+    print()
+    print("Première pizza: " + collection[0])
+    print("Dernière pizza: " + collection[-1])
         
             
     
 pizzas = ("4 fromages", "végétarienne", "hawai", "calzone")
-afficher_pizza(pizzas)
+vide = ()
+afficher_pizza(vide)
