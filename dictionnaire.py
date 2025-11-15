19653b2ee3334e7a4c68f4d3a18a46f7450fcf12
unknown <kaborejbaime@gmail.com>
2025-11-15 17:58:37 +0100

    les possibilités avec le dictionnaire

--- dictionnaire.py
+++ dictionnaire.py
@@ -1,5 +1,5 @@
 
-
+"""
 personne = {'nom': 'Mélanie', "age": 25, 'taille': 1.60}
 # print(personne)
 # print(personne['nom'])
@@ -16,3 +16,45 @@ print(personne)
 
 for i in personne:
     print(f"clef: {i} - valeur: {personne[i]}")
+
+"""
+
+# --- PARTIE 2 ---
+
+# noms, age, taille
+
+personnes = [
+    ("Mélanie", 25, 1.6),
+    ("Pierre", 27, 1.7),
+    ("Paul", 29, 1.8),
+    ("Jacques", 35, 1.75),
+    ("Martin", 16, 1.65)
+    
+]
+
+
+
+def obtenir_informations(nom, liste):
+    for i in liste:
+        if i[0] == nom:
+            return i
+    return None
+
+# Récupérer informations Jacques
+infos = obtenir_informations("Jacques", personnes)
+# print(infos)
+
+personnes_dict = {
+    "Mélanie": (25, 1.6),
+    "Pierre": (27, 1.7),
+    "Paul": (29, 1.8),
+    "Jacques": (35, 1.75),
+    "Martin": (16, 1.65)
+    
+}
+
+infos = personnes_dict.get("Jonathan")
+if not infos:
+    print("La clef n'existe pas")
+else:
+    print(infos)
