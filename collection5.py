743a0b52a18cca790aa082039572dfbc7474d550
unknown <kaborejbaime@gmail.com>
2025-11-22 17:29:38 +0100

    gestion des slices

---
+++ collection5.py
@@ -0,0 +1,15 @@
+# Les collections : Listes / Tuples
+# Slices
+
+#          0        1        2            3         4
+noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe"]
+
+# print(noms[0:2])
+# print(noms[:])
+
+slices_noms = noms[-2:]
+
+#noms[0] = "Toto"
+
+print(slices_noms)
+# print(noms)
