a56d16ac5ee9ff4834a5cd39d164ca641238678d
unknown <kaborejbaime@gmail.com>
2025-11-22 17:15:14 +0100

    append, extend, insert

---
+++ collection4.py
@@ -0,0 +1,20 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# Append / Extend / += / Insert
+
+noms = ["Jeans", "Sophie", "Martin"]
+
+noms_supplémentaires = ["Christophe", "Zoe"]
+
+# noms.append(noms_supplémentaires)
+# for e in noms_supplementaires:
+# noms.append(e)
+# noms.extend(noms_supplémentaires)
+# noms += noms_supplémentaires
+
+# noms.append("Toto")
+# noms.insert(0, "Toto")
+
+noms = noms + noms_supplémentaires
+
+
+print(noms)
