e0aba6ae2b5a895c12a910632fb7506463d72e04
unknown <kaborejbaime@gmail.com>
2026-02-08 20:12:19 +0100

    p objets introduction

--- pobjets2.py
+++ pobjets2.py
@@ -14,25 +14,27 @@
 
 # ----- DEFINITION -----
 class Personne:
-    def __init__(self): 
+    def __init__(self, nom): 
         self.nom = nom   # crée une variable d'instance : nom
         print("Constructeur personne " + nom)   
         
     def SePresenter(self):
         print("Bonjour, je m'appelle " + self.nom)
         
-#   def AutreFonction():
-€#       print("AutreFonction")
+#   def AutreFonction(self):
+#       print("Nom: " + self.nom)
 
 
 
 # ----- UTILISATION -----
 personne1 = Personne("Jean")  # Je cree une personne
-# personne2 = Personne("Paul")  # Je cree une personne
+personne2 = Personne("Paul")  # Je cree une personne
 
 # Personne.SePresenter(personne1)
 personne1.SePresenter()
-# personne2.SePresenter() # méthode d'instance
+# personne1.AutreFonction()
+personne2.SePresenter() # méthode d'instance
 
 # personne.AutreFonction()  # méthode de classe
+
 
