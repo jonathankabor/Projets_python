bc5473ee310d842ba8c89405312de9a56cfa2b54
unknown <kaborejbaime@gmail.com>
2026-02-21 17:15:55 +0100

    gestion des ingredients de la pizza

--- popizza.py
+++ popizza.py
@@ -2,16 +2,22 @@
 
 
 class Pizza:
-    def __init__(self, nom, prix):
+    def __init__(self, nom, prix, ingredients):
         self.nom = nom
         self.prix = prix
+        self.ingredients = ingredients
         
     def Afficher(self):
         print(f"PIZZA {self.nom} : {self.prix} €  ")
+        print(self.ingredients)
+        print(", ".join(self.ingredients))
         
         
         
-pizza1 = Pizza("4 fromages", 8.5)
+pizza1 = Pizza("4 fromages", 8.5, ("brie", "emmental", "compté", "parmesan"))
 pizza1.Afficher()
+
+# ingredients = ("brie", "emmental", "compté", "parmesan")
+# print(", ".join(ingredients))
 
         
