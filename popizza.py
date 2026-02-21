4641b91750d97231a3513cf1d7cb38662b9e91b9
unknown <kaborejbaime@gmail.com>
2026-02-21 17:30:29 +0100

    Ajout liste de pizza

--- popizza.py
+++ popizza.py
@@ -9,15 +9,24 @@ class Pizza:
         
     def Afficher(self):
         print(f"PIZZA {self.nom} : {self.prix} €  ")
-        print(self.ingredients)
+#       print(self.ingredients)
         print(", ".join(self.ingredients))
+        print()
         
         
         
-pizza1 = Pizza("4 fromages", 8.5, ("brie", "emmental", "compté", "parmesan"))
-pizza1.Afficher()
+# pizza1 = Pizza("4 fromages", 8.5, ("brie", "emmental", "compté", "parmesan"))
+# pizza1.Afficher()
 
 # ingredients = ("brie", "emmental", "compté", "parmesan")
 # print(", ".join(ingredients))
 
-        
+pizzas = (
+    Pizza("4 fromages", 8.5, ("brie", "emmental", "compté", "parmesan")),
+    Pizza("Hawai", 9.5, ("tomate", "ananas", "oignons")),
+    Pizza("4 saisons", 11, ("oeuf", "emmental", "tomate", "jambon")),
+    Pizza("Végétarienne", 7.8, ("champignons", "tomate", "oignons", "poivrons"))      
+)      
+
+for i in pizzas:
+    i.Afficher()
