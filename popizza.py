98c32caebe048d3271f95ef413fb2249d79ca71c
unknown <kaborejbaime@gmail.com>
2026-02-21 17:04:55 +0100

    P objets création de pizza

---
+++ popizza.py
@@ -0,0 +1,17 @@
+# Nom, prix, ingrédients, végétarienne
+
+
+class Pizza:
+    def __init__(self, nom, prix):
+        self.nom = nom
+        self.prix = prix
+        
+    def Afficher(self):
+        print(f"PIZZA {self.nom} : {self.prix} €  ")
+        
+        
+        
+pizza1 = Pizza("4 fromages", 8.5)
+pizza1.Afficher()
+
+        
