6130f64e89a2e065cc95b2f92348d29f03f9118a
unknown <kaborejbaime@gmail.com>
2026-02-22 18:05:27 +0100

    Gestion des catégories de pizza

--- popizza.py
+++ popizza.py
@@ -2,13 +2,17 @@
 
 
 class Pizza:
-    def __init__(self, nom, prix, ingredients):
+    def __init__(self, nom, prix, ingredients, vegetarienne=False):
         self.nom = nom
         self.prix = prix
         self.ingredients = ingredients
+        self.vegetarienne = vegetarienne
         
     def Afficher(self):
-        print(f"PIZZA {self.nom} : {self.prix} €  ")
+        veg_str = ""
+        if self.vegetarienne:
+            veg_str = " - VEGETARIENNE"
+        print(f"PIZZA {self.nom} : {self.prix} €  " + veg_str)
 #       print(self.ingredients)
         print(", ".join(self.ingredients))
         print()
@@ -22,10 +26,10 @@ class Pizza:
 # print(", ".join(ingredients))
 
 pizzas = (
-    Pizza("4 fromages", 8.5, ("brie", "emmental", "compté", "parmesan")),
+    Pizza("4 fromages", 8.5, ("brie", "emmental", "compté", "parmesan"), True),
     Pizza("Hawai", 9.5, ("tomate", "ananas", "oignons")),
     Pizza("4 saisons", 11, ("oeuf", "emmental", "tomate", "jambon")),
-    Pizza("Végétarienne", 7.8, ("champignons", "tomate", "oignons", "poivrons"))      
+    Pizza("Végétarienne", 7.8, ("champignons", "tomate", "oignons", "poivrons"), True)      
 )      
 
 for i in pizzas:
