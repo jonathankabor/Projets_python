bc25423826f4db13bbaeb76e59dc7ec2994da35e
unknown <kaborejbaime@gmail.com>
2026-04-05 18:51:48 +0200

    appels réseau avec requests

---
+++ Script/programme_requests.py
@@ -0,0 +1,28 @@
+# FAIRE DES APPELS RÉSEAU AVEC REQUESTS
+#
+# https://codeavecjonathan.com/res/programmation.txt
+# https://codeavecjonathan.com/res/pizzas1.json
+# https://codeavecjonathan.com/res/exemple.html
+
+import sys
+sys.path.append(r"C:\Users\Hp\Projets_python\myenv\Lib\site-packages")
+
+import requests
+import json
+
+# API REST
+"""reponse = requests.get("https://codeavecjonathan.com/res/pizzas1.json")
+if reponse.status_code == 200:
+    reponse.encoding = "utf-8"
+    print(reponse.text)
+    pizzas = json.loads(reponse.text)
+    print("Nombre de pizzas :", len(pizzas))
+else:
+    print("ERREUR code : " + str(reponse.status_code))"""
+
+reponse = requests.get("https://codeavecjonathan.com/res/exemple.html")
+if reponse.status_code == 200:
+    reponse.encoding = "utf-8"
+    print(reponse.text)
+else:
+    print("ERREUR code : " + str(reponse.status_code))
