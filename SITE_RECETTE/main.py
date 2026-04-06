cea7256577943a07e4458327c6e16904223e9668
unknown <kaborejbaime@gmail.com>
2026-04-06 17:53:48 +0200

    extraire la description

--- SITE_RECETTE/main.py
+++ SITE_RECETTE/main.py
@@ -9,8 +9,11 @@ html_content = f.read()
 f.close()
 soup = BeautifulSoup(html_content, "html.parser")
 
-titre_h1 = soup.find("h1")
+titre_h1 = soup.find("h1") # , class_=""
+paragraphe_description = soup.find("p", class_="description")
+
 
 print("Titre de la page HTML:", titre_h1.text)
+print("Paragraphe de description:", paragraphe_description.text)
 
 print()
