e0a6b4c223b4473eb7b390a9319eebb0ddd14150
unknown <kaborejbaime@gmail.com>
2026-04-12 19:30:22 +0200

    modification2

--- Script/uninstall.py
+++ Script/uninstall.py
@@ -4,12 +4,14 @@ packages_to_save = ["certifi","charset-normalizer","docutils","idna","pip","Pygm
 
 packages = open('packages.txt', 'r', encoding='utf-8', errors='ignore') 
 for i in packages:    
-    try:        true_i = i[0:i.index("\n")]    
-    except:        true_i = i            
+    try:
+        true_i = i[0:i.index("\n")]    
+    except: 
+        true_i = i            
     if true_i in packages_to_save:        
         pass    
     else:        
         try:
-            os.system(f"C:\Users\Hp\WPy64-31241\python-3.12.4.amd64\python.exe -m pip uninstall -y {true_i}")        
+            os.system(f"C:\\Users\\Hp\\WPy64-31241\\python-3.12.4.amd64\\python.exe -m pip uninstall -y {true_i}")        
         except:
             print("échec")
